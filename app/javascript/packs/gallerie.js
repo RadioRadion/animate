0f826fa93146694190087cfef523702cedf25e43
Valentin Lassartesse <valentin.lassartesse@gmail.com>
2020-04-06 16:29:37 +0200

    gallerie fixed, arrows deleted

--- app/javascript/packs/gallerie.js
+++ app/javascript/packs/gallerie.js
@@ -4,13 +4,28 @@ const bigpic3 = document.querySelector("#bigpic3 img");
 const bigpic4 = document.querySelector("#bigpic4 img");
 const bigpic5 = document.querySelector("#bigpic5 img");
 
-const pics = document.querySelectorAll(".pic img");
+const pics1 = document.querySelectorAll(".pic1 img");
+const pics2 = document.querySelectorAll(".pic2 img");
+const pics3 = document.querySelectorAll(".pic3 img");
+const pics4 = document.querySelectorAll(".pic4 img");
+const pics5 = document.querySelectorAll(".pic5 img");
 
-pics.forEach(pic => pic.addEventListener("mouseover", (event) => {
+pics1.forEach(pic => pic.addEventListener("mouseover", (event) => {
   bigpic1.src = event.currentTarget.src;
-  bigpic2.src = event.currentTarget.src;
-  bigpic3.src = event.currentTarget.src;
-  bigpic4.src = event.currentTarget.src;
-  bigpic5.src = event.currentTarget.src;
 }));
 
+pics2.forEach(pic => pic.addEventListener("mouseover", (event) => {
+  bigpic2.src = event.currentTarget.src;
+}));
+
+pics3.forEach(pic => pic.addEventListener("mouseover", (event) => {
+  bigpic3.src = event.currentTarget.src;
+}));
+
+pics4.forEach(pic => pic.addEventListener("mouseover", (event) => {
+  bigpic4.src = event.currentTarget.src;
+}));
+
+pics5.forEach(pic => pic.addEventListener("mouseover", (event) => {
+  bigpic5.src = event.currentTarget.src;
+}));
