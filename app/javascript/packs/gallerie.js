d431df08698f37fce726f6d916f8abcf05b7fcc8
Valentin Lassartesse <valentin.lassartesse@gmail.com>
2020-05-05 14:13:03 +0200

    fourth gallery added

--- app/javascript/packs/gallerie.js
+++ app/javascript/packs/gallerie.js
@@ -22,10 +22,40 @@ pics3.forEach(pic => pic.addEventListener("mouseover", (event) => {
   bigpic3.src = event.currentTarget.src;
 }));
 
-pics4.forEach(pic => pic.addEventListener("mouseover", (event) => {
-  bigpic4.src = event.currentTarget.src;
-}));
+// pics4.forEach(pic => pic.addEventListener("mouseover", (event) => {
+//   bigpic4.src = event.currentTarget.src;
+// }));
 
 pics5.forEach(pic => pic.addEventListener("mouseover", (event) => {
   bigpic5.src = event.currentTarget.src;
 }));
+
+//settime pour la gallery4'
+let count = 1;
+const timeOut = () => {
+  setTimeout(function() {
+    bigpic4.src = `assets/photos/resize/nature/0${count}.jpg`;
+    if (count === 8) {
+      count = 1;
+    } else {
+        count += 1;
+    }
+    timeOut();
+  }, 10000);
+}
+
+timeOut();
+
+//gsap for bigpic4, Bezier don't work there
+var snow = $("#bigpic4");
+var tl = new TimelineMax({repeat:-1});
+var bezierPath = [{ x: 5, y: 4 }, { x: -5, y: 8 }, { x: 5, y: 12 },{ x: -5, y: 16 }];
+
+tl.set(snow, {autoAlpha:0});
+tl.to(snow, 10, { bezier: {
+            type: 'soft',
+            values: bezierPath,
+            smoothOrigin: true,
+            timeResolution: 12
+          }, ease: Linear.easeNone});
+tl.to(snow, 5, {autoAlpha:1, repeat:1, yoyo:true, ease: Linear.easeNone}, 0);
