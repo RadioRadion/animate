1c2ee375a3d54fb67d83bff1932903e128e3b9e8
Valentin Lassartesse <valentin.lassartesse@gmail.com>
2020-06-20 11:19:05 +0200

    menu more explicit

--- app/javascript/packs/terminal.js
+++ app/javascript/packs/terminal.js
@@ -1,6 +1,6 @@
 var data = [
   {
-    AboutDevTypeText: "<span>AGONE STUDIO<br/><br/>uploading your trip<br/>. . . . . . . . . . . . . . . . . .</span><br/>"
+    AboutDevTypeText: "<span>AGONE STUDIO<br/><br/>uploading your trip<br/>. . . . . . . . . . . . . . . . . . . . . .</span><br/>"
   }
 ];
 
@@ -22,7 +22,7 @@ var allElements = document.getElementsByClassName("typeing");
       if (char === "<") isTag = true;
       if (char === ">") isTag = false;
       if (isTag) return type();
-      setTimeout(type, 120);
+      setTimeout(type, 150);
     })();
   }
 }, 2000);
